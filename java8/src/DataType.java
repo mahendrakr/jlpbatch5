da3c5a768b7295e037283a590b5b996a6d79bcb0
mahendrakr <mahendrakumar7028@gmail.com>
2022-10-27 09:05:17 -0700

    Identifiers commited by Ritika

---
+++ java8/src/DataType.java
@@ -0,0 +1,17 @@
+/**
+ * Primitive data type
+ * 1)byte
+ * 2)short
+ * 3)int
+ * 4)long
+ * 5)float
+ * 6)double
+ * 7)char
+ * 8)boolean
+ * 
+ * @author ASUS
+ *
+ */
+public class DataType {
+
+}
---
+++ java8/src/Modifiers.java
@@ -0,0 +1,19 @@
+/**
+ * 1)public
+ * 2)protected
+ * 3)private
+ * 4)static
+ * 5)synchronized
+ * 6)stricfp
+ * 7)final
+ * 8)abstract
+ * 9)native
+ * 10)transient
+ * 11)volatile
+ * 
+ * @author ASUS
+ *
+ */
+public class Modifiers {
+
+}
