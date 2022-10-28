4c26c83740618d8681316b3ff6865c7993fb0ff8
mahendrakr <mahendrakumar7028@gmail.com>
2022-10-28 08:56:00 -0700

    Modifiers implimentation commited by Ritika

--- java8/src/Modifiers.java
+++ java8/src/Modifiers.java
@@ -4,7 +4,7 @@
  * 3)private
  * 4)static
  * 5)synchronized
- * 6)stricfp
+ * 6)strictfp
  * 7)final
  * 8)abstract
  * 9)native
@@ -12,8 +12,13 @@
  * 11)volatile
  * 
  * @author ASUS
+ *Q1)Applicable modifiers for a class:final,public,abstract,strictfp[ FPAS ]
+ *Q2)Modifiers are not permitted for variables:native,abstract,synchronized;strictfp[NASS]
+ *Q3)Modifiers are not permitted for method:transient,volatile[TV]
+ *
  *
  */
-public class Modifiers {
-
+public strictfp abstract class Modifiers {
+	int age;
+	public transient void m1() {}
 }
---
+++ java8/src/Variables.java
@@ -0,0 +1,9 @@
+/**
+ * [ access modifier] [non access modifiers] <data type> <name><;>
+ * @author ASUS
+ *
+ */
+public class Variables {
+public static int age;
+public static final int a=10;
+}
