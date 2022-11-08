2eff5d743c88ecffebbbc35335b22630c9f32b52
pratiksha meena <prtkshmeena@gmail.com>
2022-11-08 19:24:23 +0530

    student class commited by sharad

--- java8/src/Student.java
+++ java8/src/Student.java
@@ -3,13 +3,19 @@ public class Student {
 	public String name="Sharad";
 	public int age=19;
 	public static String university="RGPV";
-	public int studentId=0157;
+	public int studentId=157;
 	public static String branch="CS";
 	public String collegeName="LNCT";
+	public static int YearOfAdmission=2021;
+	public static int Phone=1234567890;
+	public int dob=12/12/2002;
+	
 	
 	public static void main(String[] args) { 
 		System.out.println(Student.university);
 		System.out.println(Student.branch);
+		System.out.println(YearOfAdmission);
+		System.out.println(Phone);
 		
 		Student s= new Student();
 				
@@ -17,11 +23,7 @@ public class Student {
 		System.out.println(s.age);
 		System.out.println(s.collegeName);
 		System.out.println(s.studentId);
+		System.out.println(s.dob);
 		
-		
+		}
 	}
-	
-	
-
-
-}
