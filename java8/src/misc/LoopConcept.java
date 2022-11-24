f6dc53563ddee50e50d72b58b2d59ca74f911ea8
mahendrakr <mahendrakumar7028@gmail.com>
2022-11-24 20:57:04 +0530

    Loop committed by Riya

---
+++ java8/src/misc/LoopConcept.java
@@ -0,0 +1,16 @@
+package misc;
+
+public class LoopConcept {
+public static void main(String[] args) {
+	
+	for(int i=0;i<10;i++ ) {
+		System.out.println("Riya");
+	}
+	//for(;;);
+	//for(;;) {}
+	int i=2;
+//	do {} while(i<5 );
+int[] a= {10,20,30,40,50};	
+System.out.println(a[4]);
+}
+}
