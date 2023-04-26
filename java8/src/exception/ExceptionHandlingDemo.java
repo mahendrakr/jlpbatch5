06e735024dcccbc59468b1563652af18248f4203
pratiksha meena <prtkshmeena@gmail.com>
2023-04-26 21:03:44 +0530

    exception handling

---
+++ java8/src/exception/ExceptionHandlingDemo.java
@@ -0,0 +1,23 @@
+package exception;
+
+public class ExceptionHandlingDemo {
+	public static double division(int a,int b) {
+		double result=0;
+		try{
+			 result=a/b;
+		}
+		catch(ArithmeticException e) {
+			System.out.println("wrong input\n denominator cannot be zero");
+		}
+		return result;
+		
+	}
+	  
+	public static void main(String[] args) {
+		System.out.println("before division");
+		  double output = division(10,0);
+		  System.out.println(output);
+		  System.out.println("after division");
+	}
+
+}
