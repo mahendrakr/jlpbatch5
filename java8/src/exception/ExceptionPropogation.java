86896d02a08fdc4eb4de8b7b069ef636991c11c4
pratiksha meena <prtkshmeena@gmail.com>
2023-06-06 20:34:59 +0530

    exception

--- java8/src/exception/ExceptionPropogation.java
+++ java8/src/exception/ExceptionPropogation.java
@@ -1,19 +1,49 @@
 package exception;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class ExceptionPropogation {
-     public void m1() {
-    	 System.out.println("from m1");
-     }
-     public int m2() {
-    	 System.out.println("from m2");
-    	 return 10;
-     }
-     public void m3() {
-    	 System.out.println("from m3");
-     }
-     
+	public void m1() throws IOException {
+		System.out.println("from m1");
+		Path path = null;
+		BufferedReader br = null;
+		path = Paths.get("E:\\JAVA\\path1.txt");
+		br = Files.newBufferedReader(path);
+		while (br.ready()) {
+			String line = br.readLine();
+			System.out.println(line);
+		}
+		br.close();
+		System.out.println("from try block");
+   System.out.println("end of m1");
+	}
+
+	public int m2() throws IOException {
+		System.out.println("from m2");
+		m1();
+		System.out.println("end of m2");
+		return 10;
+	}
+
+	public void m3() throws IOException {
+		System.out.println("from m3");
+		m2();
+		System.out.println("end of m3");
+	}
+
 	public static void main(String[] args) {
-    		
+       ExceptionPropogation e =new ExceptionPropogation();
+       try {
+		e.m3();
+	} catch (IOException e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
+	}
+       System.out.println("end of main");
 	}
 
 }
--- java8/src/exception/Test.java
+++ java8/src/exception/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	private int count=0;
 	public void m1(int a,int b) {
 		try{
+			System.out.println("before division from m1");
 			int r=a/b;
 			count++;
 		}
@@ -15,13 +16,20 @@ public class Test {
 		finally {
 			count++;
 		}
-		
+	System.out.println("after division from m1");	
+	}
+	public void m2(int a,int b) {
+   System.out.println("before division");
+			int r=a/b;
+			count++;
+	System.out.println("after division");			
 	}
 	  
 
 	public static void main(String[] args) {
 		Test t=new Test();
 		t.m1(10,0);
+		t.m2(10,0);
      System.out.println(t.count);
 	}
 
